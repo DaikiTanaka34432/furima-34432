7060bafb604d4131544586e7033d2c52e42ae0df
DaikichiTanaka <t.daiki0929@gmail.com>
2021-02-17 19:01:07 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,21 @@
 class ItemsController < ApplicationController
   
   def index
+    @items =Item.all
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item =Item.new(item_params)
+  end
+
+  private 
+  def item_params
+    params.require(:item)
+    .permit(:name, :profile, :category_id,:status_id, :delivery_fee_id, :prefecture_id, :shipment_days_id, :fee, :image)
+    .merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,6 @@ class Item < ApplicationRecord
 
   has_one :purchase
   belongs_to :user
+  has_one_attached :image
 
 end
