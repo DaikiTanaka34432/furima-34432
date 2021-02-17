3b15205d510c6776a6d73f394b1e5ea01a98fb08
DaikichiTanaka <t.daiki0929@gmail.com>
2021-02-17 17:57:54 +0900

    マイグレーションの実行

---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord
+  with options presence:true do
+    validates :name
+    validates :profile
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipment_days_id
+    validates :fee
+  end
+
+  has_one :purchase
+  belongs_to :user
+
+end
