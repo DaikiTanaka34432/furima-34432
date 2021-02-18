89e50b16504ef609c663af179f4318a48c83378d
DaikichiTanaka <t.daiki0929@gmail.com>
2021-02-18 17:36:11 +0900

    出品商品・エラーメッセージの表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
-    @items =Item.all
+    @items =Item.all.order("created_at DESC")
   end
   
   def new
-    @user = current_user
+    # @user = current_user
     @item = Item.new
   end
 
   def create
-    @user = current_user.id
-    @item = @user.item.new(item_params)
+    # @user = current_user.id
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -19,10 +19,12 @@ class ItemsController < ApplicationController
     end
   end
 
+
+
   private 
   def item_params
     params.require(:item)
-    .permit(:name, :profile, :category_id,:status_id, :delivery_fee_id, :prefecture_id, :shipment_days_id, :fee, :image)
+    .permit(:name, :profile, :category_id,:status_id, :delivery_fee_id, :prefecture_id, :shipment_days_id, :fee, :image, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :shipment_days_id )
     .merge(user_id: current_user.id)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,12 +7,18 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipment_days_id
-    validates :fee
+    validates :fee,  
+      numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:'300〜9,999,999円の範囲で入力して下さい'},
+      format:{with:/\A[0-9]+\z/, message: '半角数字を使用して下さい'}
     validates :image
   end
 
   with_options numericality:{other_than: 1} do
-    validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :shipment_days_id
+    validates :category_id   
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipment_days_id
   end
 
   has_one :purchase
