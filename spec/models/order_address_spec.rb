b4a3c2105a743824f1facb4d1a4069c6b1cb1162
DaikichiTanaka <t.daiki0929@gmail.com>
2021-02-23 16:01:03 +0900

    購入機能実装の修正(2)

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
 
+      it 'item_idが空だと購入できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+
+      it 'user_idが空だと購入できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
       it 'postal_codeが空だと購入できない' do
         @order_address.postal_code = nil
         @order_address.valid?
